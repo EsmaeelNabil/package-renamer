410d509c68bf0e21a37bfb4794e7a1107bc6d2bd
GitHub Action <action@github.com>
2021-03-20 17:16:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/esmaeelnabil/packagerenamer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.esmaeelnabil.packagerenamer.services
+
+import com.github.esmaeelnabil.packagerenamer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
